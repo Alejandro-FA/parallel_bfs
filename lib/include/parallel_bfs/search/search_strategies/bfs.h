88281eea43f7535e21685aa3650b4f9d9c459606
Alejandro-FA <61871580+Alejandro-FA@users.noreply.github.com>
2024-06-06 10:19:40 +0200

    Join interruptible bfs and bfs_with_limit in a single implementation

--- lib/include/parallel_bfs/search/search_strategies/bfs.h
+++ lib/include/parallel_bfs/search/search_strategies/bfs.h
@@ -16,17 +16,21 @@
 
 
 namespace parallel_bfs::detail {
-    /// TODO: Change this from Breadth First Search to Best First Search
     /// NOTE: We use tree-like search, so we don't need to check for repeated states
     template<Searchable State, std::derived_from<BaseTransitionModel<State>> TM>
-    [[nodiscard]] std::shared_ptr<Node<State>> interruptible_bfs(std::deque<std::shared_ptr<Node<State>>> &frontier, const Problem<State, TM> &problem, std::stop_source ssource = std::stop_source{}) {
-        while (!frontier.empty() && !ssource.stop_requested()) {
+    [[nodiscard]] std::shared_ptr<Node<State>>
+    _bfs(std::deque<std::shared_ptr<Node<State>>> &frontier,
+         const Problem<State, TM> &problem,
+         std::stop_source ssource = std::stop_source{},
+         std::size_t limit = std::numeric_limits<std::size_t>::max()) {
+        while (!frontier.empty() && !ssource.stop_requested() && frontier.size() < limit) {
             auto node = frontier.front();
             frontier.pop_front();
             if (problem.is_goal(node->state())) {
                 ssource.request_stop();
                 return node;
             }
+            // frontier.push_range(problem.expand(node)); // TODO: Use this when available
             for (const auto &child: problem.expand(std::move(node))) frontier.push_back(child);
         }
 
@@ -34,24 +38,27 @@ namespace parallel_bfs::detail {
     }
 
 
+    /// NOTE: We use tree-like search, so we don't need to check for repeated states
     template<Searchable State, std::derived_from<BaseTransitionModel<State>> TM>
-    [[nodiscard]] std::shared_ptr<Node<State>> interruptible_bfs(std::shared_ptr<Node<State>> init_node, const Problem<State, TM> &problem, std::stop_source ssource = std::stop_source{}) {
+    [[nodiscard]] std::shared_ptr<Node<State>>
+    interruptible_bfs(std::deque<std::shared_ptr<Node<State>>> &frontier, const Problem<State, TM> &problem, std::stop_source ssource = std::stop_source{}) {
+        return _bfs(frontier, problem, ssource);
+    }
+
+
+    template<Searchable State, std::derived_from<BaseTransitionModel<State>> TM>
+    [[nodiscard]] std::shared_ptr<Node<State>>
+    interruptible_bfs(std::shared_ptr<Node<State>> init_node, const Problem<State, TM> &problem, std::stop_source ssource = std::stop_source{}) {
         std::deque<std::shared_ptr<Node<State>>> frontier({std::move(init_node)});
-        return interruptible_bfs(frontier, problem, ssource);
+        return _bfs(frontier, problem, ssource);
     }
 
 
     /// Search until solution is found or frontier fills up to limit
     template<Searchable State, std::derived_from<BaseTransitionModel<State>> TM>
-    [[nodiscard]] std::shared_ptr<Node<State>> bfs_with_limit(std::deque<std::shared_ptr<Node<State>>> &frontier, const Problem<State, TM> &problem, std::size_t limit) {
-        while (!frontier.empty() && frontier.size() < limit) {
-            auto node = frontier.front();
-            frontier.pop_front();
-            if (problem.is_goal(node->state())) return node;
-            // frontier.push_range(problem.expand(node)); // TODO: Use this when available
-            for (const auto &child: problem.expand(std::move(node))) frontier.push_back(child);
-        }
-        return nullptr;
+    [[nodiscard]] std::shared_ptr<Node<State>>
+    bfs_with_limit(std::deque<std::shared_ptr<Node<State>>> &frontier, const Problem<State, TM> &problem, std::size_t limit) {
+        return _bfs(frontier, problem, std::stop_source{}, limit);
     }
 }
 
